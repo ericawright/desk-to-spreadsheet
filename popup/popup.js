f146a8ee8ef6d8257c6d9389da24f55b2bbafc3b
Erica Wright <ewright@mobify.com>
2016-03-15 14:51:27 -0700

    on click store notes and action taken

--- popup/popup.js
+++ popup/popup.js
@@ -5,7 +5,15 @@ chromeps.subscribe('commands', function(message) {
         localStorage.setItem('customer_name', message['store_info'].customer_name);
     }
     if (message === 'return_local_storage') {
-        support_ticket_summary = {subject: localStorage['subject'], direct_link: localStorage['direct_link'], customer_name: localStorage['customer_name']};
+        support_ticket_summary = {subject: localStorage['subject'], direct_link: localStorage['direct_link'], customer_name: localStorage['customer_name'], action: localStorage['action'], summary: localStorage['summary']};
         chromeps.publishActive('commands', {'support_ticket_summary': support_ticket_summary});
     }
+});
+
+$('#ticket_submit').on('click', function(){
+    $(this).prop('disabled','disabled');
+    var summary = $('#ticket_summary').val();
+    var action = $('#ticket_action').val();
+    localStorage.setItem('summary', summary);
+    localStorage.setItem('action', action);
 });
